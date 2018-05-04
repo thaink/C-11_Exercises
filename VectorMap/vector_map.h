3e64f7dd542873c46b5c272a6de694e6b4c7df62
thai nguyen <thaibkset@gmail.com>
2018-05-05 08:24:08 +0900

    almost finish vector_map

--- VectorMap/vector_map.h
+++ VectorMap/vector_map.h
@@ -10,7 +10,7 @@ namespace ktstd {
     using T             = std::string;
     using Allocator     = std::allocator<std::pair<std::string,std::string>>;
     using Compare       = std::less<std::string>;
-    using value_type    = std::pair<const Key,T>;
+    using value_type    = std::pair<Key,T>;
 
     //Key = vector_map::Key
     //value_type = vector_map::value_type
@@ -44,7 +44,7 @@ namespace ktstd {
     //Allocator = std:pair<std:string,std::string>
     //template <typename Key, typename T, typename Compare = std::less<Key>,
     //		  typename Allocator = std::allocator<std::pair<const Key,T>>>
-    class vector_map : private std::vector<std::pair<const Key,T>, Allocator> {
+    class vector_map : private std::vector<std::pair<Key,T>, Allocator> {
         /*
          * ivariants:
          * - the vector_must strictly ordered.
@@ -52,10 +52,11 @@ namespace ktstd {
          * - iterator return pair<const Key, T> which does not allowed to changes Key
          */
         //basic types
+    public:
         using this_type                 = vector_map;//vector_map<Key,T,Compare,Allocator>;
         using key_type                  = Key;
         using mapped_type               = T;
-        using value_type                = std::pair<const Key,T>; //value use will see
+        using value_type                = std::pair<Key,T>; //value use will see
         using base_type                 = std::vector<value_type, Allocator>;
         using key_compare               = Compare;
         using value_compare             = vector_map_compare;//vector_map_compare<Key,value_type,Compare>
@@ -242,14 +243,249 @@ namespace ktstd {
 
     vector_map::this_type& vector_map::operator=(std::initializer_list<value_type> ilist)
     {
-        base_type.clear();
+        base_type::clear();
         insert(ilist);
+        return *this;
     }
 
     vector_map::this_type& vector_map::operator=(this_type&& x)
     {
         mCompare = x.mCompare;
         base_type::operator =(std::move(x));
+        return *this;
+    }
+
+    /*==============================================
+     * vector_map insert, erase functions
+     ==============================================*/
+
+    void vector_map::swap(this_type& x)
+    {
+        std::swap(mCompare,x.mCompare);
+        base_type::swap(x);
+    }
+
+    inline const vector_map::key_compare& vector_map::key_comp() const
+    {
+        return mCompare.c;
+    }
+
+    inline vector_map::key_compare& vector_map::key_comp()
+    {
+        return mCompare.c;
+    }
+
+    inline const vector_map::value_compare& vector_map::value_comp() const
+    {
+        return mCompare;
+    }
+
+    inline vector_map::value_compare& vector_map::value_comp()
+    {
+        return mCompare;
+    }
+
+    template <class... Args>
+    std::pair<vector_map::iterator, bool> vector_map::emplace(Args&&... args)
+    {
+        auto val = value_type(std::forward<Args>(args)...);
+        return insert(val);
+    }
+
+    template <class... Args>
+    vector_map::iterator vector_map::emplace_hint(const_iterator position, Args&&... args)
+    {
+        auto val = value_type(std::forward<Args>(args)...);
+        return insert(position,val);
+    }
+
+    std::pair<vector_map::iterator, bool> vector_map::insert(const value_type& value)
+    {
+        auto itLB = lower_bound(value.first);
+        if((itLB != end()) && !mCompare(value,*itLB))
+            return std::pair<iterator, bool>(itLB,false);
+        return std::pair<iterator, bool>(base_type::insert(itLB,value),true);
+    }
+
+    template <typename P>
+    std::pair<vector_map::iterator, bool> vector_map::insert(P&& otherValue)
+    {
+        value_type value = value_type(std::forward<P>(otherValue));
+        auto itLB = lower_bound(value.first);
+        if((itLB != end()) && !mCompare(value,*itLB))
+            return std::pair<iterator, bool>(itLB,false);
+        return std::pair<iterator, bool>(base_type::insert(itLB,std::move(value)),true);
+
+    }
+
+    vector_map::iterator vector_map::insert(const_iterator position, const value_type& value)
+    {
+        //check if position is correct
+        if ((position == end()) || mCompare(value,*position))
+            if((position == begin()) || mCompare(*(position),value))
+                return base_type::insert(position,value);
+
+        //if position is not correct
+        return insert(value).first;
+    }
+
+    vector_map::iterator vector_map::insert(const_iterator position, value_type&& value)
+    {
+        //check if position is correct
+        if ((position == end()) || mCompare(value,*position))
+            if((position == begin()) || mCompare(*(position),value))
+                return base_type::insert(position,std::move(value));
+
+        //if position is not correct
+        return insert(std::move(value)).first;
+    }
+
+    void vector_map::insert(std::initializer_list<value_type> ilist)
+    {
+        for(auto val:ilist)
+            insert(val);
+    }
+
+    template <typename InputIterator>
+    void vector_map::insert(InputIterator first, InputIterator last)
+    {
+        for(; first != last; ++first)
+            insert(*first);
+    }
+
+    vector_map::iterator  vector_map::erase(const_iterator position)
+    {
+        return base_type::erase(position);
+    }
+
+    vector_map::iterator  vector_map::erase(const_iterator first, const_iterator last)
+    {
+        return base_type::erase(first,last);
+    }
+
+    vector_map::size_type vector_map::erase(const key_type& k)
+    {
+        auto itLB = lower_bound(k);
+        if((itLB == end()) || mCompare(k,*itLB))
+            return 0;
+        base_type::erase(itLB);
+        return 1;
+    }
+
+    inline vector_map::reverse_iterator vector_map::erase(const_reverse_iterator position)
+    {
+        return reverse_iterator(base_type::erase((++position).base()));
+    }
+
+    inline vector_map::reverse_iterator vector_map::erase(const_reverse_iterator first, const_reverse_iterator last)
+    {
+        return reverse_iterator(base_type::erase((++last).base(),(++first).base()));
+    }
+
+    //access functions
+    inline vector_map::iterator  vector_map::find(const key_type& k)
+    {
+        std::pair<iterator,iterator> itPair = equal_range(k);
+        if(itPair.first != itPair.second)
+            return itPair.first;
+        return end();
+    }
+
+    inline vector_map::const_iterator vector_map::find(const key_type& k) const
+    {
+        std::pair<const_iterator,const_iterator> itPair = equal_range(k);
+        if(itPair.first != itPair.second)
+            return itPair.first;
+        return end();
+    }
+
+    template <typename U, typename BinaryPredicate>
+    vector_map::iterator vector_map::find_as(const U& u, BinaryPredicate predicate)
+    {
+        std::pair<iterator,iterator> itPair = equal_range<U,BinaryPredicate>(u,predicate);
+        if(itPair.first != itPair.second)
+            return itPair.first;
+        return end();
+    }
+
+    template <typename U, typename BinaryPredicate>
+    vector_map::const_iterator vector_map::find_as(const U& u, BinaryPredicate predicate) const
+    {
+        std::pair<const_iterator,const_iterator> itPair = equal_range<U,BinaryPredicate>(u,predicate);
+        if(itPair.first != itPair.second)
+            return itPair.first;
+        return end();
+    }
+
+    vector_map::size_type vector_map::count(const key_type& k) const
+    {
+        std::pair<const_iterator,const_iterator> itPair = equal_range(k);
+        if(itPair.first != itPair.second)
+            return 1;
+        return 0;
+    }
+
+    vector_map::iterator vector_map::lower_bound(const key_type& k)
+    {
+        return std::lower_bound(begin(), end(), k, mCompare);
+    }
+
+    vector_map::const_iterator vector_map::lower_bound(const key_type& k) const
+    {
+        return std::lower_bound(begin(), end(), k, mCompare);
+    }
+
+    vector_map::iterator  vector_map::upper_bound(const key_type& k)
+    {
+        return std::upper_bound(begin(), end(), k, mCompare);
+    }
+
+    vector_map::const_iterator vector_map::upper_bound(const key_type& k) const
+    {
+        return std::upper_bound(begin(), end(), k, mCompare);
+    }
+
+    std::pair<vector_map::iterator, vector_map::iterator>  vector_map::equal_range(const key_type& k)
+    {
+        iterator itLB = lower_bound(k);
+        if((itLB == end()) || mCompare(k,*itLB))
+            return std::pair<iterator,iterator>(itLB,itLB);
+
+        iterator itUP(itLB);
+        return std::pair<iterator,iterator>(itLB,++itUP);
+    }
+
+    std::pair<vector_map::const_iterator, vector_map::const_iterator> vector_map::equal_range(const key_type& k) const
+    {
+        const_iterator itLB = lower_bound(k);
+        if((itLB == end()) || mCompare(k,*itLB))
+            return std::pair<const_iterator,const_iterator>(itLB,itLB);
+
+        const_iterator itUP(itLB);
+        return std::pair<const_iterator,const_iterator>(itLB,++itUP);
+    }
+
+    template <typename U, typename BinaryPredicate>
+    std::pair<vector_map::iterator, vector_map::iterator>  vector_map::equal_range(const U& u, BinaryPredicate predicate)
+    {
+
+    }
+
+    template <typename U, typename BinaryPredicate>
+    std::pair<vector_map::const_iterator, vector_map::const_iterator> vector_map::equal_range(const U& u, BinaryPredicate) const
+    {
+
+    }
+
+
+    vector_map::mapped_type& vector_map::operator[](const key_type& k)
+    {
+
+    }
+
+    vector_map::mapped_type& vector_map::operator[](key_type&& k)
+    {
+
     }
 
 
